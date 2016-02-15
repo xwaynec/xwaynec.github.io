437b9cbc200a4b01d9f14d6bd9c8bc29b9e0b7a8
Wayne Chen <xwaynec@gmail.com>
2016-02-16 01:54:48 +0800

    Site updated at 2016-02-15 17:54:48 UTC

--- javascripts/tawkto.js
+++ javascripts/tawkto.js
@@ -1,4 +1,3 @@
-<script type="text/javascript">
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
   var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
@@ -8,4 +7,3 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
   s1.setAttribute('crossorigin','*');
   s0.parentNode.insertBefore(s1,s0);
 })();
-</script>
